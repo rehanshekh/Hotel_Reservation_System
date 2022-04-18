acd0f5f4ffafe4e96a9f591a5dc2155c4858417b
Rehan Shekh <rehanrshekh@gmail.com>
2022-04-18 10:08:38 +0530

    Added method to find cheapest hotel, for a date range on weekday and weekend

--- src/main/java/HotelReservation.java
+++ src/main/java/HotelReservation.java
@@ -1,15 +1,19 @@
+import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.temporal.ChronoField;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class HotelReservation {
 
     List<String> dateRange = new ArrayList<>();
     List<Hotel> hotelList = new ArrayList<>();
+
 
     public HotelReservation() {
         addHotels();
@@ -28,7 +32,7 @@ public class HotelReservation {
     private void addHotels() {
         Hotel Hotel1 = new Hotel("LakeWood", 110);
         hotelList.add(Hotel1);
-        Hotel Hotel2 = new Hotel("BridgeWood", 160);
+        Hotel Hotel2 = new Hotel("BridgeWood", 150);
         hotelList.add(Hotel2);
         Hotel Hotel3 = new Hotel("RidgeWood", 220);
         hotelList.add(Hotel3);
@@ -51,7 +55,7 @@ public class HotelReservation {
                 n.setWeekEndRates(150);
                 System.out.println("For RidgeWood Weekday & Weekend Rates per day is " + n.getRates() + " & " + n.getWeekEndRates());
                 return true;
-            } else if ((n.getRates() == 160) && (n.getWeekEndRates() == 0)) {
+            } else if ((n.getRates() == 150) && (n.getWeekEndRates() == 0)) {
                 n.setWeekEndRates(50);
                 System.out.println("For BridgeWood Weekday & Weekend Rates per day is " + n.getRates() + " & " + n.getWeekEndRates());
                 return true;
@@ -64,4 +68,35 @@ public class HotelReservation {
         });
         return hotel;
     }
+
+    public String getCheapestHotel(LocalDate startDate, LocalDate endDate,int count) {
+        int numberOfDays = (int) ChronoUnit.DAYS.between(startDate, endDate);
+        int weekends = 0;
+
+        while (startDate.compareTo(endDate) != 0) {
+            switch (DayOfWeek.of(startDate.get(ChronoField.DAY_OF_WEEK))) {
+                case SATURDAY -> ++weekends;
+                case SUNDAY -> ++weekends;
+            }
+            startDate = startDate.plusDays(1);
+        }
+
+        final int weekdaysNumber = numberOfDays - weekends;
+        final int weekendsNumber = weekends;
+
+        final double cheapestPrice = hotelList.stream().mapToDouble(hotel -> ((hotel.getWeekEndRates() * weekendsNumber + hotel.getRates() * weekdaysNumber)))
+                .min().orElse(Double.MAX_VALUE);
+
+        ArrayList<Hotel> cheapestHotel = hotelList.stream()
+                .filter(hotel -> (hotel.getWeekEndRates() * weekendsNumber + hotel.getRates() * weekdaysNumber) == cheapestPrice)
+                .collect(Collectors.toCollection(ArrayList::new));
+
+        if (count <= cheapestHotel.size()) {
+            System.out.println("Cheapest Hotel : \n" + cheapestHotel.get(count-1).getName() + ", Total Rates: " + cheapestPrice);
+            return cheapestHotel.get(count-1).getName();
+
+        }
+        return null;
+    }
 }
+
--- src/test/java/HotelReservationTest.java
+++ src/test/java/HotelReservationTest.java
@@ -1,15 +1,20 @@
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.jupiter.api.*;
 
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
 import java.util.stream.Stream;
 
+
+@TestMethodOrder(value = MethodOrderer.OrderAnnotation.class)
 public class HotelReservationTest {
     HotelReservation reservation = new HotelReservation();
-    Stream<Hotel> hotel =reservation.updateWeekendRates();
+    Stream<Hotel> hotel = reservation.updateWeekendRates();
+
     @Test
+    @Order(value=1)
     public void WhenAddedHotelData_ToHotelReservationSystem_ShouldNotReturnNull() {
         List<Hotel> hotels = reservation.getList();
         for (Hotel hotel : hotels) {
@@ -19,22 +24,42 @@ public class HotelReservationTest {
     }
 
     @Test
+    @Order(value = 2)
     public void givenDateRange_ShouldReturn_CheapestHotel() {
         LocalDate startDate = LocalDate.of(2020, Month.SEPTEMBER, 10);
         LocalDate endDate = LocalDate.of(2020, Month.SEPTEMBER, 11);
         long lowestRate = reservation.calculateCost(startDate, endDate);
         System.out.print("Total Rates: " + lowestRate);
+        System.out.println();
         Assert.assertEquals(220, lowestRate);
     }
 
     @Test
+    @Order(value = 3)
     public void WhenAddedRates_ToEachHotel_ShouldUpdateIt() {
-
-        hotel =  reservation.updateWeekendRates();
+        hotel = reservation.updateWeekendRates();
         Assert.assertEquals(90, hotel.findFirst().get().getWeekEndRates());
-        hotel =  reservation.updateWeekendRates();
+        hotel = reservation.updateWeekendRates();
         Assert.assertEquals(50, hotel.findFirst().get().getWeekEndRates());
-        hotel =  reservation.updateWeekendRates();
+        hotel = reservation.updateWeekendRates();
         Assert.assertEquals(150, hotel.findFirst().get().getWeekEndRates());
     }
+
+
+    @Test
+    @Order(4)
+    public void ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ() {
+        int count=1;
+        WhenAddedHotelData_ToHotelReservationSystem_ShouldNotReturnNull();
+        givenDateRange_ShouldReturn_CheapestHotel();
+        WhenAddedRates_ToEachHotel_ShouldUpdateIt();
+        LocalDate startDate = LocalDate.of(2020, Month.SEPTEMBER, 11);
+        LocalDate endDate = LocalDate.of(2020, Month.SEPTEMBER, 13);
+        String cheapestHotel = reservation.getCheapestHotel(startDate, endDate, count);
+        Assert.assertEquals("LakeWood", cheapestHotel);
+        count++;
+        cheapestHotel = reservation.getCheapestHotel(startDate,endDate,count);
+        Assert.assertEquals("BridgeWood",cheapestHotel);
+    }
+
 }
